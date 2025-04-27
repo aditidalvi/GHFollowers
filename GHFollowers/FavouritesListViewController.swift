3f1eaefa2a1a1cfafbaa80e6d7ae9feec49653ce
aditidalvi <aditidalvi22@gmail.com>
2025-04-27 18:48:44 +0530

    cretaed view controllers

---
+++ GHFollowers/FavouritesListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FavouritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Aditi Dalvi on 27/04/25.
+//
+
+import UIKit
+
+class FavouritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
