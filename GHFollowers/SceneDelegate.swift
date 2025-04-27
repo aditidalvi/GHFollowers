f05ae6d5db0c4aaba5e4384b72cd4d6cfada9deb
aditidalvi <aditidalvi22@gmail.com>
2025-04-27 16:54:56 +0530

    created a initial set up in scene delegate and view controller

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -16,7 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // willConnectTo - is similar to didFinishLaunchingWithOptions used to be in App Delegate
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds) // make it fit to the entire screen.
+        window?.windowScene = windowScene // every window has a window scene.
+        window?.rootViewController = ViewController() // ViewController is the root view controller that is used to display our screens. window needs a viewcontroller to show.
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
